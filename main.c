1076d250f2288bd5026f630d8c8631c8d2737453
Petr Kučera <45851215+petrkucerak@users.noreply.github.com>
2020-10-24 18:49:56 +0200

    Add fce find_optimal_q

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define REPS 1000
+#define REPS 100
 
 int is_prime_num(int number)
 {
@@ -19,13 +19,46 @@ int is_prime_num(int number)
     return 1;
 }
 
+int gcd(int numH, int numL)
+{
+    
+
+    while(numA != 0){
+        
+    }
+
+    return 0;
+}
+
+int find_optimal_q(int numH, int numL)
+/** numH - number higher
+ * numL - number lower
+ * metoda hleda nejblizsiho nasobitele v N
+ **/
+{
+    int q = 1;
+
+    while((numH / ( (q+1) * numL)) != 0){
+        ++ q;
+    }
+
+    return q;
+}
+
 int main(int argc, char *argv[])
 {   
-    for(int i = 0; i < REPS; ++ i){
+    /*for(int i = 0; i < REPS; ++ i){
         if(is_prime_num(i) == 1){
             printf("%d\n", i);
         }
-    }
+    }*/
+
+    int a = 358;
+    int b = 374;
+
+    int q = find_optimal_q(a,b);
+    fprintf(stdout,"%d\n",q);
+
 
     return 0;
 }
@@ -34,6 +67,7 @@ int main(int argc, char *argv[])
 
 ToDo:
 - je prvocislo
+- euklidovsky alogoritmus -> gcd - nejvetsi spolecny delitel
 - rozklad cisla na prvocisla
 - gcd a gca
 - bezoutova identita
