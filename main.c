d6572001d3e1f9b5f3ab4e66ba8864156b65fa96
Petr Kučera <45851215+petrkucerak@users.noreply.github.com>
2020-10-24 20:07:55 +0200

    Graf vypsani Euklidova algoritmu, Bezrut identity

--- main.c
+++ main.c
@@ -19,19 +19,9 @@ int is_prime_num(int number)
     return 1;
 }
 
-int gcd(int numH, int numL)
-{
-    
-
-    while(numA != 0){
-        
-    }
-
-    return 0;
-}
-
 int find_optimal_q(int numH, int numL)
-/** numH - number higher
+/**
+ * numH - number higher
  * numL - number lower
  * metoda hleda nejblizsiho nasobitele v N
  **/
@@ -45,6 +35,74 @@ int find_optimal_q(int numH, int numL)
     return q;
 }
 
+int find_gcd(int numH, int numL)
+/**
+ * vraci gcd pro kladna cisla
+ * treba drzet poradi zadavani
+ * tj. numH - vyssi cislo
+ * numL nizsi cislo
+ **/
+{
+    int q;
+    int numL_new;
+
+    while(numL != 0){
+        q = find_optimal_q(numH, numL);
+        //fprintf(stdout,"q = %d; numH = %d; numL = %d\n", q, numH, numL);
+        numL_new = numH - (q * numL);
+        numH = numL;
+        numL = numL_new;
+    }
+
+    return numH;
+}
+
+void the_bezout_identity(int numH, int numL)
+{
+
+    int a = numH;
+    int b = numL;
+
+    int q;
+    int value_help;
+    
+    int x_H = 1;
+    int x_L = 0;
+
+    int y_H = 0;
+    int y_L = 1;
+
+    fprintf(stdout,"EUKLIDUV ALGORITMUS\n");
+
+    fprintf(stdout,"|%10d | %10d | %10d |\n", numH, x_H, y_H);
+    fprintf(stdout,"|%10d | %10d | %10d |\n", numL, x_L, y_L);
+    fprintf(stdout,"=======================================\n");
+
+    while(numL != 0){
+        q = find_optimal_q(numH, numL);
+        value_help = numH - (q * numL);
+        numH = numL;
+        numL = value_help;
+
+        value_help = x_H - (x_L * q);
+        x_H = x_L;
+        x_L = value_help;
+
+        value_help = y_H - (y_L * q);
+        y_H = y_L;
+        y_L = value_help;
+
+        //fprintf(stdout,"%d | %d | %d\n", numH, x_H, y_H);
+        fprintf(stdout,"|%10d | %10d | %10d |\n", numL, x_L, y_L);
+    }
+
+    int gcd = numH;
+
+    fprintf(stdout,"\ngcd(%d,%d) = %d\n\n", a, b, gcd);
+    fprintf(stdout,"Bezoutova identita:\n");
+    fprintf(stdout,"%d * (%d) + %d * (%d) = %d\n", a, x_H, b, y_H, gcd);
+}
+
 int main(int argc, char *argv[])
 {   
     /*for(int i = 0; i < REPS; ++ i){
@@ -54,11 +112,15 @@ int main(int argc, char *argv[])
     }*/
 
     int a = 358;
-    int b = 374;
+    int b = 217;
 
     int q = find_optimal_q(a,b);
     fprintf(stdout,"%d\n",q);
 
+    int gcd = find_gcd(a, b);
+    fprintf(stdout,"gcd(%d,%d): %d\n", a, b, gcd);
+
+    the_bezout_identity(a, b);
 
     return 0;
 }
@@ -71,5 +133,6 @@ ToDo:
 - rozklad cisla na prvocisla
 - gcd a gca
 - bezoutova identita
+- doselchtit code!!!
 
 ============= */
